a999e2db640992cb9c2d96abbc7b171c4d2be177
jndean8007-rgb <jndean8007@gmail.com>
2026-08-01 07:28:31 -0700

    hotfixtest

--- Data/Data_Cleaning.py
+++ Data/Data_Cleaning.py
@@ -7,8 +7,10 @@ from Import_Raw import pc
 import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import URL, create_engine, text
-
+#awdoawudh
 pc = Pc()
 df = pd.read_sql("""
 
 """, con = pc.engine)
+
+print('Hello world righty now')
